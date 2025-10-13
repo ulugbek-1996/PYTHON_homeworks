9b0fa5a64d2202264a74e344e1da19e160ae1cc9
ulugbek-1996 <u.panjiev.uznim@gmail.com>
2025-10-13 22:33:34 +0500

    Create hw1.py

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,76 @@
+#1-Task
+
+# Input: side of the square
+side = float(input("Enter the side length of the square: "))
+
+# Calculate perimeter and area
+perimeter = 4 * side
+area = side * side  # or side ** 2
+
+# Output the results
+print(f"Perimeter of the square: {perimeter}")
+print(f"Area of the square: {area}");
+
+#2-Task
+
+from math import pi
+
+# Input: diameter of circle
+diametr = float(input("Enter the diameter of circle: "))
+
+# Calculate lenght of circle
+length = pi * diametr
+
+# Output the results
+print(f"Lenght of circle: {length}");
+
+#3-Task
+
+a = float(input('Enter the value of a: '))
+
+b = float(input('Enter the value of b: '))
+
+mean = (a + b)/2
+
+print(f'The mean of a and b :{mean}');
+
+#4-Task
+
+a = float(input('Enter the value of a: '))
+
+b = float(input('Enter the value of b: '))
+
+sum = a + b  
+
+product = a * b  
+
+square_a = a ** 2 
+
+square_b = b ** 2 
+
+print(f'The sum of a and b :{sum}')
+
+print(f'The product of a and b :{product}')
+
+print(f'The square of a :{square_a}')
+
+print(f'The square of b :{square_b}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
